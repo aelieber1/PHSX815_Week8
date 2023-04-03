29f95d38a7fd2e76b2077202b7721bd1d579c4bb
aelieber1 <80122879+aelieber1@users.noreply.github.com>
2023-04-03 03:02:57 -0500

    fix to minimization procedure

--- Homework10.py
+++ Homework10.py
@@ -17,13 +17,8 @@ Although none of these are explicitly required for this homework, these are addi
 - Can you compare different algorithms in the minimization? Do you observe any differences?
 
  
-
-Some possible minimization libraries/methods you can use (with examples):
-
-[Python]
-
+Some possible minimization libraries/methods you can use (with examples): [Python]
 - https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html
-
 - https://scipy-lectures.org/advanced/mathematical_optimization/index.html
 
 
@@ -44,7 +39,8 @@ from mpl_toolkits.mplot3d import Axes3D
 # define function with 2 variables
 #def obj(x,y):
    #return np.sin(x)*np.cos(y)
-def obj(x,y):
+def obj(params):
+    x, y = params
     return 2*x**2 - x*y + 2*y**2
 
 # visualize function
@@ -53,10 +49,11 @@ x = np.linspace(-100,100,100)
 y = np.linspace(-100,100,100)
 
 # Create an array of calues containing all pairs (xi,yi) for xi in x and yi in y
+XY = np.meshgrid(x, y)
 X, Y = np.meshgrid(x, y)
 
 # Compute vlaues of our function for each defined pair
-Z = obj(X,Y)
+Z = obj(XY)
 
 # Visualize plot
 fig = plt.figure()
@@ -70,5 +67,16 @@ ax.set_zlabel("f(x,y)", fontsize = 10)
 plt.show()
 
 # minimize function
-#res = minimize(obj, (2.5,5), method='BFGS')
-#print(res)
+initial_guess = [0,0]
+res = minimize(obj, initial_guess, method='BFGS')
+# error check help from stack overflow (https://stackoverflow.com/questions/13670333/multiple-variables-in-scipys-optimize-minimize)
+if res.success:
+    fitted_params = res.x
+    print("Minimum found at: ", fitted_params)
+    print("Full Minimization Details: \n", res)
+else:
+    raise ValueError(result.message)
+    
+"""
+Full explanation of all of the attributes from the OptimizeResult object found here in the Scipy docs: https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.OptimizeResult.html#scipy.optimize.OptimizeResult
+"""
